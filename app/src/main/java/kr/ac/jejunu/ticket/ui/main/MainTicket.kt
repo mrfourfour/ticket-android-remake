5de6310b19e729bf53bdff5d1c11d8e5852d085d
soohyeon13 <rlatngus6663@gmail.com>
2020-10-13 18:30:39 +0900

    리뷰 반영

--- app/src/main/java/kr/ac/jejunu/ticket/ui/main/MainTicket.kt
+++ app/src/main/java/kr/ac/jejunu/ticket/ui/main/MainTicket.kt
@@ -6,8 +6,9 @@ import androidx.databinding.DataBindingUtil
 import kr.ac.jejunu.ticket.R
 import kr.ac.jejunu.ticket.databinding.ActivityMainTicketBinding
 
-private lateinit var binding : ActivityMainTicketBinding
-class MainTicket :AppCompatActivity(){
+private lateinit var binding: ActivityMainTicketBinding
+
+class MainTicket : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
